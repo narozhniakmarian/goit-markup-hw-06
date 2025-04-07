238f847455aeddcfbcf2515134f8f5df73cb3628
Marian Narozhniak <narozhniakmarian@gmail.com>
2025-04-07 22:34:38 +0200

    js

--- js/menu.js
+++ js/menu.js
@@ -1,35 +1,44 @@
-// const openMenuBtn = document.getElementById('openMenuBtn');
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const openMenuBtn = document.getElementById('openMenuBtn');
+//   // **Додати id="openMenuBtn до кнопки "Відкрити меню" в HTML**
+//   // **Додати id="closeMenuBtn до кнопки "Закрити меню" в HTML**
+//   // **Додати id="mobileMenu" до div з класом "mobile-menu" в HTML**
 // const closeMenuBtn = document.getElementById('closeMenuBtn');
 // const mobileMenu = document.getElementById('mobileMenu');
 
-// openMenuBtn.addEventListener('click', () => {
-//   mobileMenu.classList.add('active');
-// });
 
-// closeMenuBtn.addEventListener('click', () => {
-//   mobileMenu.classList.remove('active');
-// });
+//   openMenuBtn.onclick = function () {
+//     mobileMenu.style.display = 'block';
+//   };
 
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
- const openMenuBtn = document.getElementById('openMenuBtn');
-const closeMenuBtn = document.getElementById('closeMenuBtn');
-const mobileMenu = document.getElementById('mobileMenu');
-
-
-  openMenuBtn.onclick = function () {
-    mobileMenu.style.display = 'block';
-  };
-
-  closeMenuBtn.onclick = function () {
-    mobileMenu.style.display = 'none';
-  };
+//   closeMenuBtn.onclick = function () {
+//     mobileMenu.style.display = 'none';
+//   };
 
 //   window.onclick = function (event) {
 //     if (event.target === modal) {
 //       modal.style.display = 'none';
 //     }
 //   };
-});
+// });
+
+(() => {
+  const refs = {
+    // Додати атрибут data-modal-open на кнопку відкриття
+    openModalBtn: document.querySelector('[data-menu-open]'),
+    // Додати атрибут data-modal-close на кнопку закриття
+    closeModalBtn: document.querySelector('[data-menu-close]'),
+    // Додати атрибут data-modal на бекдроп модалки
+    modal: document.querySelector('[data-menu]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.modal.classList.toggle('is-open');
+  }
+})();
--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,43 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const modal = document.getElementById('myModal');
-  const openBtn = document.getElementById('openModalBtn');
-  const closeBtn = document.querySelector('.btn-close');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const modal = document.getElementById('myModal');
+//  **додати id="myModal" до div з класом modal**
+//   const openBtn = document.getElementById('openModalBtn');
+// **додати id="openModalBtn" до кнопки з класом btn-open**
+//   const closeBtn = document.querySelector('.btn-close');
 
-  openBtn.onclick = function () {
-    modal.style.display = 'block';
+//   openBtn.onclick = function () {
+//     modal.style.display = 'block';
+//   };
+
+//   closeBtn.onclick = function () {
+//     modal.style.display = 'none';
+//   };
+
+//   window.onclick = function (event) {
+//     if (event.target === modal) {
+//       modal.style.display = 'none';
+//     }
+//   };
+// });
+
+
+
+
+(() => {
+  const refs = {
+   
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    
+    modal: document.querySelector("[data-modal]"),
   };
 
-  closeBtn.onclick = function () {
-    modal.style.display = 'none';
-  };
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  window.onclick = function (event) {
-    if (event.target === modal) {
-      modal.style.display = 'none';
-    }
-  };
-});
+  function toggleModal() {
+   
+    refs.modal.classList.toggle("is-open");
+  }
+})();
