641177aea69f8aeda900b75f55e0eace650bfa85
Marian Narozhniak <narozhniakmarian@gmail.com>
2025-04-10 20:33:58 +0200

    over

--- js/over-text-scroll.js
+++ js/over-text-scroll.js
@@ -7,16 +7,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const handleScroll = () => {
     const middle = window.innerHeight / 2;
 
-    overCards.forEach((card) => {
-      const overText = card.querySelector('.over-text');
-      const rect = card.getBoundingClientRect();
+      overCards.forEach((card) => {
+          const overText = card.querySelector('.over-text');
+          const rect = card.getBoundingClientRect();
 
-      if (rect.top < middle && rect.bottom > middle) {
-        overText.style.transform = 'translateY(0)';
-      } else {
-        overText.style.transform = 'translateY(100%)';
+          if (rect.top < middle && rect.bottom > middle) {
+              overText.style.transform = 'translateY(0)';
+          } else {
+              overText.style.transform = 'translateY(100%)';
+          }
       }
-    });
+    );
   };
 
   
@@ -30,3 +31,56 @@ document.addEventListener('DOMContentLoaded', () => {
 
   window.addEventListener('scroll', handleScroll);
 });
+
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   if (window.innerWidth >= 1158) return;
+
+//   const overCards = document.querySelectorAll('.over-card');
+
+//   const handleScroll = () => {
+//     const middle = window.innerHeight / 2;
+
+//     overCards.forEach((card) => {
+//       const overText = card.querySelector('.over-text');
+//       const rect = card.getBoundingClientRect();
+
+//       const isManuallyHidden = overText.classList.contains('manually-hidden');
+
+//       if (rect.top < middle && rect.bottom > middle) {
+//         if (!isManuallyHidden) {
+//           overText.style.transform = 'translateY(0)';
+//         }
+//       } else {
+//         if (!isManuallyHidden) {
+//           overText.style.transform = 'translateY(100%)';
+//         }
+//       }
+//     });
+//   };
+
+//   overCards.forEach((card) => {
+//     const overText = card.querySelector('.over-text');
+
+//     overText.addEventListener('click', () => {
+//       const computedTransform = getComputedStyle(overText).transform;
+
+//       const matrix = new WebKitCSSMatrix(computedTransform);
+//       const currentTranslateY = matrix.m42; 
+
+//       const isVisible = currentTranslateY === 0;
+
+//       if (isVisible) {
+//         overText.style.transform = 'translateY(100%)';
+//         overText.classList.add('manually-hidden');
+//       } else {
+//         overText.style.transform = 'translateY(0)';
+//         overText.classList.remove('manually-hidden');
+//       }
+//     });
+//   });
+
+//   window.addEventListener('scroll', handleScroll);
+// });
